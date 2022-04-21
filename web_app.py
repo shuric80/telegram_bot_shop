955909f6a7026dc18cc33b5706d4b38401990375
batenov <abatenov@ivi.ru>
2022-04-21 16:59:48 +0300

    update web_app

--- web_app.py
+++ web_app.py
@@ -4,6 +4,7 @@ from aiohttp.web_fileresponse import FileResponse
 from aiohttp.web_response import Response
 
 from config import settings
+from utils import parse_init_data
 
 routes = web.RouteTableDef()
 
@@ -15,5 +16,27 @@ async def index(request) -> Response:
 @routes.post('/submitOrder') -> Response:
 async def submit_order(request):
     data = await request.json()
-    init_data = parse_init_data()
+    init_data = parse_init_data(token=settings.BOT_TOKEN, raw_init_data=data['initData'])
+    if not init_data:
+        return
+    bot: Bot = request.app['bot']
+    query_id = init_data['query_id']
+
+    result_text = '<b>Order summary</b>'
+    for item in data['items']:
+        name, price, amount = item.values()
+        result_text += f'{name} x{amount} - <b>{price}</b>'
+
+    result_text += '\n'+ data['totalPrice']
+
+    result = types.InlineQueryResultArticle(
+        id=query_id,
+        title='Order',
+        input_message_content=types.InputTextMessageContent(message_text=result_text),
+    )
+
+    await bot.answer_web_app_query(query_id, result)
+
+
+
 
